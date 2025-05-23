c7210c937403f183e978109e15031e3b3d29550e
Daniil Furkalo <daniilf077@gmail.com>
2025-05-23 10:07:58 +0300

    commit 6

---
+++ tests/test_sanity.py
@@ -0,0 +1,14 @@
+def test_sanity():
+    assert 1 + 1 == 2
+
+def test_sanity2():
+    assert 1 + 1 == 2
+
+def test_sanity3():
+    assert 1 + 1 == 2
+
+def test_sanity4():
+    assert 1 + 1 == 2
+
+def test_sanity5():
+    assert 1 + 1 == 2
