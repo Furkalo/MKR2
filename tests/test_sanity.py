2f6600edb7858c619a2431bc56390fc200c01eb2
Daniil Furkalo <daniilf077@gmail.com>
2025-05-23 10:14:57 +0300

    commit 7

--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -1,14 +1,44 @@
-def test_sanity():
-    assert 1 + 1 == 2
+# tests.py
+import os
+import django
 
-def test_sanity2():
-    assert 1 + 1 == 2
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_gallery.settings')
+django.setup()
 
-def test_sanity3():
-    assert 1 + 1 == 2
 
-def test_sanity4():
-    assert 1 + 1 == 2
+from django.test import TestCase
+from gallery.models import Category, Image
+from django.core.files.uploadedfile import SimpleUploadedFile
+import datetime
 
-def test_sanity5():
-    assert 1 + 1 == 2
+
+class CategoryModelTest(TestCase):
+
+    def test_category_str(self):
+        category = Category.objects.create(name='Nature')
+        self.assertEqual(str(category), 'Nature')
+
+
+class ImageModelTest(TestCase):
+
+    def setUp(self):
+        self.category = Category.objects.create(name='Art')
+        self.image_file = SimpleUploadedFile(
+            name='test_image.jpg',
+            content=b'small test image',
+            content_type='image/jpeg'
+        )
+
+    def test_image_creation(self):
+        image = Image.objects.create(
+            title='Test Image',
+            image=self.image_file,
+            age_limit=18
+        )
+        image.categories.add(self.category)
+
+        self.assertEqual(str(image), 'Test Image')
+        self.assertEqual(image.age_limit, 18)
+        self.assertEqual(image.categories.count(), 1)
+        self.assertEqual(image.categories.first().name, 'Art')
+        self.assertEqual(image.created_date, datetime.date.today())
